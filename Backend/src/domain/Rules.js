b2d6b48b9c8a1828208ed2bf0b6a90914be988f9
Cédric <86252008+popille@users.noreply.github.com>
2026-03-26 00:01:14 +0100

    correction pour ajout de la valeur sur as

--- Backend/src/domain/Rules.js
+++ Backend/src/domain/Rules.js
@@ -1,23 +1,11 @@
 // rules.js
 export function calculateHandValue(cards) {
   let total = 0;
-  let aces = 0;
 
-  for (const card of cards) {
-    if (card.suit === "A") {
-      aces++;
-      total += card.value;
-    } else if (["K", "Q", "J"].includes(card.rank)) {
-      total += 10;
-    } else {
-      total += parseInt(card.rank);
-    }
-  }
+  cards.forEach((card) => {
+    total += parseInt(card.value);
+  });
 
-  while (total > 21 && aces > 0) {
-    total -= 10;
-    aces--;
-  }
   return total;
 }
 
